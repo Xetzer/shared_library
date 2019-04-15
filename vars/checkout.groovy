c5e772213187b7054f3e352c7afdd440ac917c26
Ihar_Lahutka <Ihar_Lahutka@epam.com>
2019-04-15 16:29:46 +0300

    initial

---
+++ vars/checkout.groovy
@@ -0,0 +1,19 @@
+//
+def call(String app = '', Map config = [:]) {
+  checkout scm: [
+    $class : 'GitSCM',
+    branches : [[name: "${config.branch}"]],
+    doGenerateSubmoduleConfigurations: false,
+      [$class: 'LocalBranch', localBranch: "${config.branch}"],
+      [$class: 'PruneStaleBranch'],
+      [$class: 'CleanCheckout'],
+    ],
+    userRemoteConfigs: [
+      [
+        url: "${config.gitlabUrl}/${config.application}.git",
+      ]
+    ]
+  ]
+}
+
+return this;
