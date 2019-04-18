2089d6cef7afc92fdbb599d19b7cb2f0b00e289d
Ihar_Lahutka <Ihar_Lahutka@epam.com>
2019-04-18 09:03:55 +0300

    readme

--- vars/createmap.groovy
+++ vars/createmap.groovy
@@ -1,14 +1,13 @@
 import groovy.transform.Field
 
+//Map config = [application: 'Ihar.Lahutka/nr-publication-directory-app', branch: 'integration-ES1.7', buildTool: 'gradle', runSonar: false, multiModule: false]
+def call(String app = '') {
+  return config.app 
+}
+
 @Field
-Map config = [
+def config = [
         "nr-elasticsearch17-app" : [application: 'newsroom-apps/nr-elasticsearch17-app', runSonar: false, branch: 'integration', buildTool: 'gradle'],
         "nr-publication-directory-app" : [application: 'newsroom-apps/nr-publication-directory-app', runSonar: true, branch: 'integration-ES1.7', buildTool: 'gradle']
 ]
-//Map config = [application: 'Ihar.Lahutka/nr-publication-directory-app', branch: 'integration-ES1.7', buildTool: 'gradle', runSonar: false, multiModule: false]
-def call(String app = '') {
-  value = config.app
-  println value
-}
 
-return this;
