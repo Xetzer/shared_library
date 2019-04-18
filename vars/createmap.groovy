817357b74cce6c0c49fdf9ea43decff2384117ad
Ihar_Lahutka <Ihar_Lahutka@epam.com>
2019-04-18 09:29:55 +0300

    readme

--- vars/createmap.groovy
+++ vars/createmap.groovy
@@ -6,8 +6,7 @@ def call(String app = '') {
 
 @Field
 def config = [
-        "nr-elasticsearch17-app" : [application: 'newsroom-apps/nr-elasticsearch17-app', runSonar: false, branch: 'integration', buildTool: 'gradle', multimo],
-        "nr-publication-directory-app" : [application: 'newsroom-apps/nr-publication-directory-app', runSonar: true, branch: 'integration-ES1.7', buildTool: 'gradle']
+        "nr-elasticsearch17-app" : [application: 'newsroom-apps/nr-elasticsearch17-app', runSonar: false, branch: 'integration', buildTool: 'gradle', multimodule: false],
+        "nr-publication-directory-app" : [application: 'newsroom-apps/nr-publication-directory-app', runSonar: true, branch: 'integration-ES1.7', buildTool: 'gradle', multimodule: false]
 ]
 
-return this;
