985928a51dcd507ccae49291f03406673c980173
Ihar_Lahutka <Ihar_Lahutka@epam.com>
2019-04-19 15:53:18 +0300

    readme

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,5 +1,5 @@
 //String app = '',
-def call(Map config = [:], String branch = '', String snapshot = '') {
+def call(String snapshot = '') {
   print app
    def artifact_name = (snapshot == '') ? 'release' : 'snapshot'
    return artifact_name
