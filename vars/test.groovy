fa87783a3240221f0c5a201aba55ff8f7eed4c34
Ihar_Lahutka <Ihar_Lahutka@epam.com>
2019-04-28 19:42:03 +0300

    test bool

--- vars/test.groovy
+++ vars/test.groovy
@@ -4,7 +4,7 @@ def call(Map config = [:], String snapshot = '', String deploy = 'false') {
    //def artifact_name = (snapshot == '') ? 'release' : 'snapshot'
    def artifact_name = (deploy == 'true') ? 'deploy' : 'dont_deploy'
    println artifact_name
-   return artifact_name
+   //return artifact_name
 }
 
 return this;
